080c8a076085116283007bcf17bc055774c240f1
MarcoFloresCa <marcoangeloflorescantillana@gmail.com>
2026-03-03 08:59:55 -0300

    Fix main.py import path

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from src.main import main
+import os
+import sys
 import asyncio
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+from src.main import main
 
 asyncio.run(main())
